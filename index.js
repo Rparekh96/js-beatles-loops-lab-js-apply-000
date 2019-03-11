c521a661e8e9b598b3b8fb633607055b594d03c0
Ronak Parekh <rparekh1996@gmail.com>
2019-03-11 20:50:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles (n) {
   const arr = []
   do {
-    arr.push('I love the beatles!')
+    arr.push 'I love the beatles!')
     n++
   } while (n < 15)
   
